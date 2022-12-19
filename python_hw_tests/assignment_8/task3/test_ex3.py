07c4b9ba600ce9b932036ca08545b0a3c921fd2f
Alexandru Andrita <andu.andritza@gmail.com>
2022-12-19 21:29:13 +0200

    test_task3_corrected

--- python_hw_tests/assignment_8/task3/test_ex3.py
+++ python_hw_tests/assignment_8/task3/test_ex3.py
@@ -10,6 +10,12 @@ class TestAggregator(unittest.TestCase):
         int_agg(4,"string",5.1,90)
         self.assertEqual(int_agg(),100)
 
+    def test_sum_dobule(self):
+        int_agg=ex3.Aggregator(agg_type=float)
+        int_agg(4.5,8.7,-5.6,1.2)
+        int_agg("test ",-6.7,1.2)
+        self.assertEqual(int_agg(),3.299999999999999)
+
     def test_sum_stings(self):
         int_agg=ex3.Aggregator(agg_type=str,ignore_errors=False)
         int_agg("this"," ","is a test")
